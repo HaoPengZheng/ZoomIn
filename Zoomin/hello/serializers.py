cdd02de8d0d2400de82aca8062471b9cb50620a1
郑浩鹏 <354416479@qq.com>
2018-06-07 20:01:56 +0800

    删除目录Zoomin

--- Zoomin/hello/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from hello.models import User
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'created', 'username', 'password')
-
