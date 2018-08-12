53dc09a0f28d2eaa4021e5b1f0a4d359aa83f747
郑浩鹏 <354416479@qq.com>
2018-08-12 16:02:47 +0800

    no commit message

---
+++ src/components/task-release/mychart.vue
@@ -0,0 +1,40 @@
+<template>
+  <!-- 一个用来展示图表的组件 -->
+  <div style="margin-bottom:20px">
+    <el-card class="box-card" :body-style="{padding:'20px'}" >
+      <div :id="domId" style="width:100%;height:400px;">111 </div>
+    </el-card>
+  </div>
+</template>
+<script>
+export default {
+  props:{
+    domId:String,
+    option:Object
+    
+  },
+  mounted:function(){
+    this.drawLine();
+  },
+  methods: {
+    drawLine() {
+      // 基于准备好的dom，初始化echarts实例
+      let myChart = this.$echarts.init(document.getElementById(this.domId));
+      // 绘制图表
+      myChart.setOption(this.option);
+    }
+  }
+};
+</script>
+
+
+ <style>
+.text {
+  font-size: 14px;
+}
+
+.item {
+  padding: 18px 0;
+}
+
+</style>
---
+++ src/components/task-release/taskRelease.vue
@@ -0,0 +1,308 @@
+<template>
+  <el-container>
+    <Left :active="activeIndex" v-on:updateDetail="updateDetail">
+    </Left>
+    <el-container>
+      <el-main style="margin-top:20px;padding:0 10%;">
+        <div class="task-title">
+          <div class="task-title-name">
+            任务名
+          </div>
+          <div class="task-title-new">
+            <el-button type="primary" icon="el-icon-picture">新建图表</el-button>
+          </div>
+          <div style="clear:both"></div>
+        </div>
+        <el-row :gutter="20">
+          <el-col :span="12">
+            <MyChart :domId="'id1'" :option="option"></MyChart>
+          </el-col>
+          <el-col :span="12">
+            <MyChart :domId="'id2'" :option="option2"></MyChart>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="12">
+            <MyChart :domId="'id3'" :option="option3"></MyChart>
+          </el-col>
+          <el-col :span="12">
+            <MyChart :domId="'id4'" :option="option4"></MyChart>
+          </el-col>
+        </el-row>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+<script>
+import Left from "../common/task_left.vue";
+import MyChart from "./mychart.vue";
+export default {
+  components: {
+    Left,
+    MyChart
+  },
+  data() {
+    return {
+      taskId: "",
+      option: {
+        xAxis: {
+          type: "category",
+          data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+        },
+        yAxis: {
+          type: "value"
+        },
+        series: [
+          {
+            data: [820, 932, 901, 934, 1290, 1330, 1320],
+            type: "line"
+          }
+        ]
+      },
+      option2: {
+        title: {
+          text: "某地区蒸发量和降水量",
+          subtext: "纯属虚构"
+        },
+        tooltip: {
+          trigger: "axis"
+        },
+        legend: {
+          data: ["蒸发量", "降水量"]
+        },
+        toolbox: {
+          show: true,
+          feature: {
+            dataView: { show: true, readOnly: false },
+            magicType: { show: true, type: ["line", "bar"] },
+            restore: { show: true },
+            saveAsImage: { show: true }
+          }
+        },
+        calculable: true,
+        xAxis: [
+          {
+            type: "category",
+            data: [
+              "1月",
+              "2月",
+              "3月",
+              "4月",
+              "5月",
+              "6月",
+              "7月",
+              "8月",
+              "9月",
+              "10月",
+              "11月",
+              "12月"
+            ]
+          }
+        ],
+        yAxis: [
+          {
+            type: "value"
+          }
+        ],
+        series: [
+          {
+            name: "蒸发量",
+            type: "bar",
+            data: [
+              2.0,
+              4.9,
+              7.0,
+              23.2,
+              25.6,
+              76.7,
+              135.6,
+              162.2,
+              32.6,
+              20.0,
+              6.4,
+              3.3
+            ],
+            markPoint: {
+              data: [
+                { type: "max", name: "最大值" },
+                { type: "min", name: "最小值" }
+              ]
+            },
+            markLine: {
+              data: [{ type: "average", name: "平均值" }]
+            }
+          },
+          {
+            name: "降水量",
+            type: "bar",
+            data: [
+              2.6,
+              5.9,
+              9.0,
+              26.4,
+              28.7,
+              70.7,
+              175.6,
+              182.2,
+              48.7,
+              18.8,
+              6.0,
+              2.3
+            ],
+            markPoint: {
+              data: [
+                { name: "年最高", value: 182.2, xAxis: 7, yAxis: 183 },
+                { name: "年最低", value: 2.3, xAxis: 11, yAxis: 3 }
+              ]
+            },
+            markLine: {
+              data: [{ type: "average", name: "平均值" }]
+            }
+          }
+        ]
+      },
+      option3: {
+        title: {
+          text: "某站点用户访问来源",
+          subtext: "纯属虚构",
+          x: "center"
+        },
+        tooltip: {
+          trigger: "item",
+          formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        legend: {
+          orient: "vertical",
+          left: "left",
+          data: ["直接访问", "邮件营销", "联盟广告", "视频广告", "搜索引擎"]
+        },
+        series: [
+          {
+            name: "访问来源",
+            type: "pie",
+            radius: "55%",
+            center: ["50%", "60%"],
+            data: [
+              { value: 335, name: "直接访问" },
+              { value: 310, name: "邮件营销" },
+              { value: 234, name: "联盟广告" },
+              { value: 135, name: "视频广告" },
+              { value: 1548, name: "搜索引擎" }
+            ],
+            itemStyle: {
+              emphasis: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: "rgba(0, 0, 0, 0.5)"
+              }
+            }
+          }
+        ]
+      },
+      option4: {
+        backgroundColor: "#2c343c",
+
+        title: {
+          text: "Customized Pie",
+          left: "center",
+          top: 20,
+          textStyle: {
+            color: "#ccc"
+          }
+        },
+
+        tooltip: {
+          trigger: "item",
+          formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+
+        visualMap: {
+          show: false,
+          min: 80,
+          max: 600,
+          inRange: {
+            colorLightness: [0, 1]
+          }
+        },
+        series: [
+          {
+            name: "访问来源",
+            type: "pie",
+            radius: "55%",
+            center: ["50%", "50%"],
+            data: [
+              { value: 335, name: "直接访问" },
+              { value: 310, name: "邮件营销" },
+              { value: 274, name: "联盟广告" },
+              { value: 235, name: "视频广告" },
+              { value: 400, name: "搜索引擎" }
+            ].sort(function(a, b) {
+              return a.value - b.value;
+            }),
+            roseType: "radius",
+            label: {
+              normal: {
+                textStyle: {
+                  color: "rgba(255, 255, 255, 0.3)"
+                }
+              }
+            },
+            labelLine: {
+              normal: {
+                lineStyle: {
+                  color: "rgba(255, 255, 255, 0.3)"
+                },
+                smooth: 0.2,
+                length: 10,
+                length2: 20
+              }
+            },
+            itemStyle: {
+              normal: {
+                color: "#c23531",
+                shadowBlur: 200,
+                shadowColor: "rgba(0, 0, 0, 0.5)"
+              }
+            },
+
+            animationType: "scale",
+            animationEasing: "elasticOut",
+            animationDelay: function(idx) {
+              return Math.random() * 200;
+            }
+          }
+        ]
+      }
+    };
+  },
+  created: function() {},
+  methods: {
+    fetchTaskById: function() {
+      this.$axios.post("");
+    },
+    handleClick(tab, event) {
+      console.log(tab, event);
+    }
+  }
+};
+</script>
+
+<style>
+  .task-title{
+    border-bottom: 2px solid #ccc;
+    padding-bottom: 5px;
+    margin-bottom: 20px;
+  }
+  .task-title-new{
+    float: right;
+  }
+  .task-title-name{
+    float: left;
+    line-height: 42px;
+    font-size: 20px;
+    font-weight: 550;
+    color: #1f2f3d;
+    font-family: "Microsoft YaHei","微软雅黑",Arial,sans-serif;
+  }
+</style>
+
