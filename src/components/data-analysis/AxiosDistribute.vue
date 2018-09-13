e862e62a26103c551453d232c82728a0e77315fc
CJW <649341167@qq.com>
2018-09-13 22:06:07 +0800

    no commit message

--- src/components/data-analysis/AxiosDistribute.vue
+++ src/components/data-analysis/AxiosDistribute.vue
@@ -19,22 +19,10 @@ export default {
     },
     created() {
       this.dataSetId = this.$route.params.dataSetId;
-      //alert(this.dataSetId)
-      //此处拿到id
+
     },
     mounted(){
-        // axios.get('http://120.79.146.91:8000/task/scoreAnalysis')
-        //     .then(function (response) {
-        //         //console.log(response.data.data);
-        //         Bus.$emit('AxiosDataDragItem', response.data.data[0]);
-        //         Bus.$emit('AxiosDataEcharts', response.data.data);
-        //     })
-        //     .catch(function (response) {
-        //        // console.log(response);
-        //     });
 
-
-        //alert(this.dataSetId)
       this.$axios
         .post(
           "http://120.79.146.91:8000/task/dataProcessing/showDataSet1",
@@ -49,13 +37,39 @@ export default {
           }
         )
         .then(response => {
-          //console.log(response.data.data);
             Bus.$emit('AxiosDataDragItem', response.data.data[0]);
             Bus.$emit('AxiosDataEcharts', response.data.data);
+        })
+        .catch(response => {
+          alert("获取数据失败");
+        });
+
+
+
+
+        this.$axios
+        .post(
+          "http://120.79.146.91:8000/task/chart/sum",
+          {
+              chart_id:11,
+              data_set:4,
+              chart_type:1,
+              x_axis:"性别_x,考生号",
+              y_axis:"RANK,RANK_H"
+          },
+          {
+            headers: {
+              Authorization: "JWT " + localStorage.getItem("token")
+            }
+          }
+        )
+        .then(response => {
+          //console.log(response.data.data);
+           console.log(response)
 
         })
         .catch(response => {
-          //alert("获取数据失败");
+          alert("response");
         });
 
 
@@ -63,12 +77,12 @@ export default {
         .post(
           "http://120.79.146.91:8000/chart/",
           {
-              data_set : 22,
-              title : "柱状图2122",
+              data_set : 2334,
+              title : "柱状图9134",
               desc: "zzzzz",
               chart_type: 1,
-              x_axis: "性别_x，考生号",
-              y_axis: "RANK，RANK_H",
+              x_axis: "性别_x,考生号",
+              y_axis: "RANK,RANK_H",
               contrast_axis: "",
               secondary_axis: ""
           },
@@ -79,7 +93,7 @@ export default {
           }
         )
         .then(response => {
-
+            console.log(response)
         })
         .catch(response => {
           //alert("获取数据失败");
