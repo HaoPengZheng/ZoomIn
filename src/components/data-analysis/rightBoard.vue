2f83c530cfcab200b8221fa600a1055d93908b59
CJW <649341167@qq.com>
2018-10-11 15:20:39 +0800

    no commit message

--- src/components/data-analysis/rightBoard.vue
+++ src/components/data-analysis/rightBoard.vue
@@ -1,30 +1,68 @@
 <template>
-  <div id="rightBoard">
-    <vue-scroll>
+  <div id="rightBoard" style="height:500px;position:relaitve;">
+    <el-scrollbar>
       <div style="margin:10px;">
-        <el-collapse v-model="activeNames">
-          <el-collapse-item title="图表标题" name="1">
-            <right-title/>
-          </el-collapse-item>
-          <el-collapse-item title="图表类型" name="2">
-            <rightChartSetting/>
-          </el-collapse-item>
-          <el-collapse-item title="图表样式" name="3" v-show="featureConfigurationFlag">
-            <chartStyle/>
-          </el-collapse-item>
-          <el-collapse-item title="功能配置" name="4" v-show="featureConfigurationFlag">
-            <featureConfiguration/>
-          </el-collapse-item>
-          <el-collapse-item title="图内筛选器" name="5">
-            <picFilter/>
-          </el-collapse-item>
-          <el-collapse-item title="图表备注" name="6">
-            <rightTips/>
-          </el-collapse-item>
-        </el-collapse>
-        <el-button @click="nextClick">下一步</el-button>
+        <el-button  type="success" plain style="width:100%;" @click="nextClick">下一步</el-button>
+        
+<draggable>			
+				
+   
+
+        <el-card style="margin-top:10px;margin-bottom:10px">
+          <div slot="header" class="clearfix">
+            <span style="font-family:微软雅黑;font-size:14px;">图表标题</span>
+            <el-button style="float: right; padding: 3px 0" type="text"><i class="el-icon-setting"></i></el-button>
+          </div>
+          <right-title/>
+        </el-card>
+
+        <el-card style="margin-top:10px;margin-bottom:10px">
+          <div slot="header" class="clearfix">
+            <span style="font-family:微软雅黑;font-size:14px;">图表类型</span>
+            <el-button style="float: right; padding: 3px 0" type="text"><i class="el-icon-setting"></i></el-button>
+          </div>
+          <rightChartSetting/>
+        </el-card>
+
+        <el-card style="margin-top:10px;margin-bottom:10px">
+          <div slot="header" class="clearfix">
+            <span style="font-family:微软雅黑;font-size:14px;">图表样式</span>
+            <el-button style="float: right; padding: 3px 0" type="text"><i class="el-icon-setting"></i></el-button>
+          </div>
+          <chartStyle/>
+        </el-card>
+
+        <el-card style="margin-top:10px;margin-bottom:10px">
+          <div slot="header" class="clearfix">
+            <span style="font-family:微软雅黑;font-size:14px;">功能配置</span>
+            <el-button style="float: right; padding: 3px 0" type="text"><i class="el-icon-setting"></i></el-button>
+          </div>
+          <featureConfiguration/>
+        </el-card>
+
+        <el-card style="margin-top:10px;margin-bottom:10px">
+          <div slot="header" class="clearfix">
+            <span style="font-family:微软雅黑;font-size:14px;">图内筛选器</span>
+            <el-button style="float: right; padding: 3px 0" type="text"><i class="el-icon-setting"></i></el-button>
+          </div>
+          <picFilter/>
+        </el-card>
+
+        <el-card style="margin-top:10px;margin-bottom:10px">
+          <div slot="header" class="clearfix">
+            <span style="font-family:微软雅黑;font-size:14px;">图表备注</span>
+            <el-button style="float: right; padding: 3px 0" type="text"><i class="el-icon-setting"></i></el-button>
+          </div>
+          <rightTips/>
+        </el-card>
+			
+
+</draggable>
+
+
+
       </div>
-    </vue-scroll>
+    </el-scrollbar>
   </div>
 </template>
 
@@ -35,22 +73,23 @@ import rightTitle from "./rightOption/rightTitle";
 import rightChartSetting from "./rightOption/rightChartSetting";
 import featureConfiguration from "./rightOption/featureConfiguration";
 import chartStyle from "./rightOption/chartStyle";
-// import picFilter from './rightOption/picFilter'
+import draggable from 'vuedraggable' 
+import picFilter from './rightOption/picFilter'
 
 export default {
   components: {
     rightTips,
     rightTitle,
     rightChartSetting,
+    picFilter,
     featureConfiguration,
     chartStyle,
-
+    draggable
   },
   data() {
     return {
       input: "",
       featureConfigurationFlag: false,
-      activeNames: ["1", "2", "3", "4", "5"]
     };
   },
   mounted() {
@@ -76,7 +115,7 @@ export default {
         this.winWidth = document.documentElement.clientWidth;
 
       //DIV高度为浏览器窗口的高度
-      //document.getElementById("rightBoardScrollbar").style.height = this.winHeight*0.95	 + "px";
+      document.getElementById("rightBoard").style.height = this.winHeight*0.95	 + "px";
       //console.log(document.getElementById("rightBoardScrollbar").style.height)
     },
     nextClick() {
